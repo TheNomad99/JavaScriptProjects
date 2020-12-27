b984a6df3b3c586860aa8b7da431633ffafaa164
TheNomad99 <ynur29@gmail.com>
2020-12-27 22:19:56 +0100

    update to TodoApp

--- TodoApp/index.js
+++ TodoApp/index.js
@@ -3,3 +3,12 @@ const items = document.querySelectorAll(".item");
 items.forEach( (e) =>{
         e.addEventListener('click', () => console.log(event.target.className))
 })
+
+function Handle(event) {
+    //...
+}
+
+function modal() {
+    //...
+}
+
