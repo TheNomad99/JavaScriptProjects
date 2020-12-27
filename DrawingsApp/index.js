aec76b44ed1b38101c7b3e67c6bd6a5248e2fc63
TheNomad99 <ynur29@gmail.com>
2020-12-27 18:34:37 +0100

    update to DrawingApp

---
+++ DrawingsApp/index.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("canvas");
+
+let clear = false;
+let isPressed = false;
+
+const ctx = canvas.getContext("2d");
+
+canvas.addEventListener("mousedown", () => {});
