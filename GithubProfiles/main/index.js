7315205058f14c34b5be2c127346dc42590fc2f5
TheNomad99 <ynur29@gmail.com>
2021-03-09 21:23:22 +0100

    Update to Githu profiles
    change in:
    - html file
    - js file
    - css file

--- GithubProfiles/main/index.js
+++ GithubProfiles/main/index.js
@@ -1,9 +1,34 @@
-$.get("https://api.github.com/users/TheNomad99",function(data,status){
-    console.log(status);
-    console.log(data["login"]);
-    const profile = `<div href="${data["html_url"]}" class="profile">
-     <h3>${data["name"]}</h3>
-     <span >@${data["login"]}</span>
-    </div>`;
-    $("#table").html(profile);    
+
+function GetUserInfo(params) {
+    $.get("https://api.github.com/users/TheNomad99",function(data,status){
+     //   console.log(status);
+        let profile="";
+        $.map(data,(e) => {
+            // profile += `<div href="${e.html_url}" class="profile">
+            // <h3>${e.name}</h3>
+            // <span>@${e.login}</span>
+            // </div>`;
+            console.log(`Username:${data["login"]} \nName:${data["name"]}\nUrl:${data["html_url"]}`);
+        }) 
+        console.log(profile);
+        // $("#table").html(profile);    
+})    
+}
+
+GetUserInfo();
+let username = "w3cj";
+//
+$.get(`https://api.github.com/users/${username}/repos`, function(data, status){
+   // console.log(status);
+   if(data.length > 0){
+    let ut = "";
+    data.map((e) => {
+        ut += `<tr><td><a target="_blank" href="${e.html_url}">${e.name}</a></td><td>${e.language}</td></tr>`;
+    })
+    $("tbody").html(ut);   
+   } else {
+       $("#display").css("display","block")
+   }
+    
 })
+
