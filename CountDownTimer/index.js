002c9b52acb072f92d9326d3378fac167ee1a474
TheNomad99 <ynur29@gmail.com>
2021-03-30 14:16:46 +0200

    change in index.js

--- CountDownTimer/index.js
+++ CountDownTimer/index.js
@@ -5,11 +5,11 @@
 //   console.log(Math.floor(Math.abs(days)/7));
 
 let CountingFromDate = new Date("Jan 1, 2021 00:00:00");
-const Week = document.getElementById("weeks");
-const Days = document.getElementById("days");
-const Hours = document.getElementById("hours");
-const Minutes = document.getElementById("minutes");
-const Seconds = document.getElementById("seconds");
+// const Week = document.getElementById("weeks");
+// const Days = document.getElementById("days");
+// const Hours = document.getElementById("hours");
+// const Minutes = document.getElementById("minutes");
+// const Seconds = document.getElementById("seconds");
 const x = setInterval(function(){
     let now = new Date().getTime();
 
@@ -20,10 +20,10 @@ const x = setInterval(function(){
     let hours = Math.abs(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
     let minutes = Math.abs(Math.floor((distance % (1000*60*60))/(1000*60)));
     let seconds = Math.abs(Math.floor((distance % (1000 * 60)) / 1000));
-    // console.log(weeks+"-"+days+"d - "+hours+"h - "+minutes+"m - "+seconds+"s");
-    Week.innerHTML="0"+weeks;
-    Days.innerHTML=days;
-    Hours.innerHTML=hours;
-    Minutes.innerHTML=minutes;
-    Seconds.innerHTML=seconds;
+    console.log(weeks+"-"+days+"d - "+hours+"h - "+minutes+"m - "+seconds+"s");
+    // Week.innerHTML="0"+weeks;
+    // Days.innerHTML=days;
+    // Hours.innerHTML=hours;
+    // Minutes.innerHTML=minutes;
+    // Seconds.innerHTML=seconds;
 },1000);
